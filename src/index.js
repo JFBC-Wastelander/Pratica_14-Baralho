92bf9f3ccaa885a2aab9a4b145ed497629fbce00
Jean <JFBimCopinski@gmail.com>
2024-08-19 10:35:30 -0300

    init: express.json

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ const express = require("express")
 const naipe_controller = require("./controllers/naipe.js")
 const app = express()
 const port = 3000
+app.use(express.json())
 
 app.post("/naipe", (req, res) => {
     const naipe = req.body
